020dced89a76b7b9f82f7dda9a9f017bb1fada98
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-01 23:44:07 +0300

    Create channelsSlice

--- frontend/src/store/slices/channelsSlice.js
+++ frontend/src/store/slices/channelsSlice.js
@@ -1,20 +1,45 @@
 import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
+import fetchDataThunk from './fetchDataThunk.js';
 
 const channelsAdapter = createEntityAdapter();
 
-const initialState = channelsAdapter.getInitialState();
+const statusName = {
+  loading: 'loading',
+  loaded: 'loaded',
+  notloaded: 'notloaded',
+};
+
+const initialState = channelsAdapter.getInitialState({
+  currentChannelId: 1,
+  statusbar: null,
+  errors: null,
+});
 
 const channelsSlice = createSlice({
   name: 'channels',
   initialState,
   reducers: {
     addChannel: channelsAdapter.addOne,
+    removeChannel: channelsAdapter.removeOne,
   },
-  /* extraReducers: (builder) => {
-    builder.addCase()
-  }, */
+  extraReducers: (builder) => {
+    builder.addCase(fetchDataThunk.pending, (state) => {
+      state.statusbar = statusName.loading;
+      state.errors = null;
+    })
+      .addCase(fetchDataThunk.fulfilled, (state, action) => {
+        state.statusbar = statusName.loaded;
+        state.errors = null;
+        channelsAdapter.setAll(state, action.payload.channels);
+      })
+      .addCase(fetchDataThunk.rejected, (state, action) => {
+        state.statusbar = 'failed';
+        state.errors = action.error;
+      });
+  },
 });
 
 export const { actions } = channelsSlice;
-export const selectors = channelsAdapter.getSelectors((state) => state.channels);
+export const channelsSelector = channelsAdapter.getSelectors((state) => state.channels);
+
 export default channelsSlice.reducer;
