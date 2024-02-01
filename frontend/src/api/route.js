b5f0ab0e3a31e8ff2df211b29c4efbe184afd44d
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-01 22:09:25 +0300

    Delete some files (channelsApi, route)

--- frontend/src/api/route.js
+++
@@ -1,7 +0,0 @@
-const apiPath = '/api/v1';
-
-export default {
-  loginPath: () => [apiPath, 'login'].join('/'),
-  channelsPath: () => [apiPath, 'channels'].join('/'),
-  messagesPath: () => [apiPath, 'messages'].join('/'),
-};
