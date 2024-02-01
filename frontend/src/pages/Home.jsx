c54ffb414dc5226f44f9d5ef68de683516927076
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-01 22:12:14 +0300

    Corrected Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,44 +1,40 @@
 // import axios from 'axios';
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { Container } from 'react-bootstrap';
 /* import ROUTES from './route.jsx';
 import useAuth from '../hooks/index.js';
 import store from '../store/index.js';
 import apiRoutes from '../api/route.js'; */
-import getAuthHeader from '../utils/getAuthHeader.js';
-import { useGetChannelsQuery } from '../store/services/channelsApi.js';
+// import getAuthHeader from '../utils/getAuthHeader.js';
+import ChannelsContainer from '../containers/ChannelsContainer.jsx';
+import MessagesContainer from '../containers/MessagesContainer.jsx';
+import { channelsSelector } from '../store/slices/channelsSlice.js';
+import fetchDataThunk from '../store/slices/fetchDataThunk.js';
 
 const Home = () => {
-  const token = getAuthHeader();
-  const [getChannels] = useGetChannelsQuery();
-  const { data } = useGetChannelsQuery();
   const dispatch = useDispatch();
-
   useEffect(() => {
-    dispatch(getChannels(token));
-    console.log(data);
-  }, []);
+    const fetchData = async () => {
+      try {
+        await dispatch(fetchDataThunk());
+      } catch (error) {
+        console.error('Error fetching channels:', error);
+      }
+    };
 
+    fetchData();
+  }, [dispatch]);
+  const ch = useSelector(channelsSelector.selectAll);
+  console.log(ch);
   return (
-    <div>
-      <header>
-        {data && (
-          <div>
-            {/* Здесь используйте данные */}
-            {data.map((item) => (
-              <div key={item.id}>{item.name}</div>
-            ))}
-          </div>
-        )}
-        <a
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex flex-column h-100">
+      <Container className="h-100 my-4 overflow-hidden rounded shadow">
+        <div className="row h-100 bg-white flex-md-row">
+          <ChannelsContainer />
+          <MessagesContainer />
+        </div>
+      </Container>
     </div>
   );
 };
