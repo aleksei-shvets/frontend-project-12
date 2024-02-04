e077301f5227e70f727ae5272e3433a80fbe4a58
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-04 15:42:20 +0300

    Create messagesSlice

--- frontend/src/store/slices/messagesSlice.js
+++ frontend/src/store/slices/messagesSlice.js
@@ -1,8 +1,19 @@
 import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
+import fetchDataThunk from './fetchDataThunk.js';
 
 const messagesAdapter = createEntityAdapter();
 
-const initialState = messagesAdapter.getInitialState();
+const statusName = {
+  loading: 'loading',
+  loaded: 'loaded',
+  failed: 'failed',
+};
+
+const initialState = messagesAdapter.getInitialState({
+  currentChannelId: null,
+  statusbar: null,
+  errors: null,
+});
 
 const messagesSlice = createSlice({
   name: 'messages',
@@ -10,11 +21,23 @@ const messagesSlice = createSlice({
   reducers: {
     addMessages: messagesAdapter.addOne,
   },
-  /* extraReducers: (builder) => {
-    builder.addCase()
-  }, */
+  extraReducers: (builder) => {
+    builder.addCase(fetchDataThunk.pending, (state) => {
+      state.statusbar = statusName.loading;
+      state.errors = null;
+    })
+      .addCase(fetchDataThunk.fulfilled, (state, action) => {
+        state.statusbar = statusName.loaded;
+        state.errors = null;
+        messagesAdapter.setAll(state, action.payload.data.messages);
+      })
+      .addCase(fetchDataThunk.rejected, (state, action) => {
+        state.statusbar = 'failed';
+        state.errors = action.error;
+      });
+  },
 });
 
 export const { actions } = messagesSlice;
-export const selectors = messagesAdapter.getSelectors((state) => state.messages);
+export const messagesSelectors = messagesAdapter.getSelectors((state) => state.messages);
 export default messagesSlice.reducer;
