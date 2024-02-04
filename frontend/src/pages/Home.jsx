c29de000679685abd06b7ada43e5122dd9178ee3
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-04 15:43:33 +0300

    Correct Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,32 +1,24 @@
-// import axios from 'axios';
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
-/* import ROUTES from './route.jsx';
-import useAuth from '../hooks/index.js';
-import store from '../store/index.js';
-import apiRoutes from '../api/route.js'; */
-// import getAuthHeader from '../utils/getAuthHeader.js';
+// import Spinner from 'react-bootstrap/Spinner';
 import ChannelsContainer from '../containers/ChannelsContainer.jsx';
 import MessagesContainer from '../containers/MessagesContainer.jsx';
-import { channelsSelector } from '../store/slices/channelsSlice.js';
+// import { channelsSelector, currentChannelIdSelector } from '../store/slices/channelsSlice.js';
 import fetchDataThunk from '../store/slices/fetchDataThunk.js';
+// import store from '../store/index.js';
 
 const Home = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        await dispatch(fetchDataThunk());
-      } catch (error) {
-        console.error('Error fetching channels:', error);
-      }
+      await dispatch(fetchDataThunk());
     };
 
     fetchData();
   }, [dispatch]);
-  const ch = useSelector(channelsSelector.selectAll);
-  console.log(ch);
+
   return (
     <div className="d-flex flex-column h-100">
       <Container className="h-100 my-4 overflow-hidden rounded shadow">
