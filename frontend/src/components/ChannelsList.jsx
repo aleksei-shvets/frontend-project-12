3250ee965afceaa59c7c471a075e03534b3626a2
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-04 15:44:36 +0300

    Correct ChannelsList component

--- frontend/src/components/ChannelsList.jsx
+++ frontend/src/components/ChannelsList.jsx
@@ -1,16 +1,36 @@
-import { ListGroup } from 'react-bootstrap';
-import { RemovebleItem, UnremovebleItem } from './channelsListItem.jsx';
+// import { ListGroup } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
-const ChannelsList = ({ channels, handleChoose }) => (
-  <ListGroup as="ul" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
+const ChannelsList = ({ channels, handleChoose, currentChannelId }) => (
+  <ul className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
     {channels.map((channel) => (
-      <ListGroup.Item as="li" key={channel.id} className="nav-item w-100">
+      <li key={channel.id} action className="bg-light nav-item w-100">
         {channel.removeble
-          ? <RemovebleItem channel={channel} handleClick={handleChoose} />
-          : <UnremovebleItem channel={channel} handleClick={handleChoose} />}
-      </ListGroup.Item>
+          ? (
+            <Button
+              onClick={handleChoose}
+              type="button"
+              variant={currentChannelId === channel.id ? 'secondary' : 'light'}
+              className="text-start w-100 rounded-0"
+            >
+              <span className="me-1">#</span>
+              {channel.name}
+            </Button>
+          )
+          : (
+            <Button
+              onClick={handleChoose}
+              type="button"
+              variant={currentChannelId === channel.id ? 'secondary' : 'light'}
+              className="w-100 rounded-0 text-start"
+            >
+              <span className="me-1">#</span>
+              {channel.name}
+            </Button>
+          )}
+      </li>
     ))}
-  </ListGroup>
+  </ul>
 );
 
 export default ChannelsList;
