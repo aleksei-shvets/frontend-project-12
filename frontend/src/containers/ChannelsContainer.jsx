7a36cb2e7e4bf616c63c32ec5ac68c18fadbeae4
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-04 15:45:24 +0300

    Correct ChannelsContainer component

--- frontend/src/containers/ChannelsContainer.jsx
+++ frontend/src/containers/ChannelsContainer.jsx
@@ -1,15 +1,23 @@
 import { useSelector } from 'react-redux';
+import { channelsSelector, currentChannelIdSelector } from '../store/slices/channelsSlice.js';
 import ChannelsHeader from '../components/ChannelsHeader.jsx';
 import ChannelsList from '../components/ChannelsList.jsx';
-import { channelsSelector } from '../store/slices/channelsSlice.js';
 
 const ChannelsContainer = () => {
+  const channels = useSelector((state) => channelsSelector.selectAll(state));
+  const currentChannelId = useSelector((state) => currentChannelIdSelector(state));
+  /* const currentChannel = useSelector(
+    (state) => channelsSelector.selectById(state, currentChannelId),
+  ); */
   const handleChoose = () => { };
-  const channels = useSelector(channelsSelector.selectAll);
   return (
     <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
       <ChannelsHeader />
-      <ChannelsList handleChoose={handleChoose} channels={channels} />
+      <ChannelsList
+        handleChoose={handleChoose}
+        channels={channels}
+        currentChannelId={currentChannelId}
+      />
     </div>
   );
 };
