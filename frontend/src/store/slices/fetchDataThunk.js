94a8fbcad1de54e6d666abf2aff9eeede76b7918
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-06 17:29:07 +0300

    Mini refactoring

--- frontend/src/store/slices/fetchDataThunk.js
+++ frontend/src/store/slices/fetchDataThunk.js
@@ -3,12 +3,10 @@ import axios from 'axios';
 import getAuthHeader from '../../utils/getAuthHeader.js';
 import ROUTES from '../../fetchApi/route.js';
 
-const token = getAuthHeader();
-
 const fetchDataThunk = createAsyncThunk(
   'channels/fetchChannels',
   async () => {
-    const response = await axios.get(ROUTES.dataPath(), { headers: token });
+    const response = await axios.get(ROUTES.dataPath(), { headers: getAuthHeader() });
     return response;
   },
 );
