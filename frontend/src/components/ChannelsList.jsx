0031555b461b4d77fb0461d3a4d339add3f0f7a7
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-10 13:55:29 +0300

    Coorecned variant attribut for buttons

--- frontend/src/components/ChannelsList.jsx
+++ frontend/src/components/ChannelsList.jsx
@@ -5,14 +5,14 @@ import { Button } from 'react-bootstrap';
 const ChannelsList = ({ channels, handleChoose, currentChannelId }) => (
   <ul className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
     {channels.map((channel) => (
-      <li key={channel.id} action className="bg-light nav-item w-100">
+      <li key={channel.id} className="bg-light nav-item w-100">
         {channel.removeble
           ? (
             <Button
               id={channel.id}
               onClick={() => handleChoose(channel.id)}
               type="button"
-              variant={(currentChannelId === channel.id ? 'secondary' : 'light')}
+              variant={(Number(currentChannelId) === Number(channel.id) ? 'secondary' : 'light')}
               className="text-start w-100 rounded-0"
             >
               <span className="me-1">#</span>
@@ -24,7 +24,7 @@ const ChannelsList = ({ channels, handleChoose, currentChannelId }) => (
               id={channel.id}
               onClick={() => handleChoose(channel.id)}
               type="button"
-              variant={(currentChannelId === channel.id ? 'secondary' : 'light')}
+              variant={(Number(currentChannelId) === Number(channel.id) ? 'secondary' : 'light')}
               className="w-100 rounded-0 text-start"
             >
               <span className="me-1">#</span>
