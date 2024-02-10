c16d659232bdfb4dc5df777ad4c8db3e97d279f9
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-10 23:26:05 +0300

    Correct keys-id for message items

--- frontend/src/components/MessageItem.jsx
+++ frontend/src/components/MessageItem.jsx
@@ -1,5 +1,5 @@
 export const CurrentUserMessage = ({ item }) => (
-  <div key={item?.id} className="container pl-3 mb-4 row text-end">
+  <div className="container pl-3 mb-4 row text-end">
     <div className="col-lg-2" />
     <div className="col-10">
       <p className="fw-bold m-0 small px-3">{item?.username}</p>
@@ -11,7 +11,7 @@ export const CurrentUserMessage = ({ item }) => (
 );
 
 export const OtherUsersMessage = ({ item }) => (
-  <div key={item?.id} className="container pl-3 mb-4 row">
+  <div className="container pl-3 mb-4 row">
     <div className="col-10">
       <p className="fw-bold m-0 small px-3">{item?.username}</p>
       <div className="rounded px-3 py-1 m-0 bg-primary-subtle text-break">
