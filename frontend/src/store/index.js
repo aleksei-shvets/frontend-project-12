83daf96459e2383902626609258ef0c96c304582
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-10 15:36:13 +0300

    Add modalSlice

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer from './slices/channelsSlice.js';
 import messagesReducer from './slices/messagesSlice.js';
 import userReducer from './slices/userSlice.js';
+import modalReducer from './slices/modalSlice.js';
 
 export default configureStore({
   reducer: {
     channels: channelsReducer,
     messages: messagesReducer,
     user: userReducer,
+    modal: modalReducer,
   },
 });
---
+++ frontend/src/store/slices/modalSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isShown: false,
+  type: null,
+};
+
+const modalSlice = createSlice({
+  name: 'modal',
+  initialState,
+  reducers: {
+    showModal: (state) => {
+      state.isShown = true;
+    },
+    hideModal: (state) => {
+      state.isShown = false;
+    },
+    setModalType: (state, modalType) => {
+      state.type = modalType;
+    },
+  },
+});
+
+export const isShownSelector = (state) => state.modal.isShown;
+export const { actions: modalActions } = modalSlice;
+export default modalSlice.reducer;
