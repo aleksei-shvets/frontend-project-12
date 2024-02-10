675e783fa7c56b1af91d9e720fdccd5cc595a7f0
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-10 15:38:54 +0300

    Add handler for open modal

--- frontend/src/containers/ChannelsContainer.jsx
+++ frontend/src/containers/ChannelsContainer.jsx
@@ -2,10 +2,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { channelsSelector, currentChannelIdSelector, channelActions } from '../store/slices/channelsSlice.js';
 import ChannelsHeader from '../components/ChannelsHeader.jsx';
 import ChannelsList from '../components/ChannelsList.jsx';
+// import useModal from '../hooks/useModal.js';
+import { modalActions } from '../store/slices/modalSlice.js';
 
-// import store from '../store/index.js';
+import store from '../store/index.js';
 
 const ChannelsContainer = () => {
+  // const modalHook = useModal();
   const dispath = useDispatch();
   const channels = useSelector((state) => channelsSelector.selectAll(state));
   const currentChannelId = useSelector((state) => currentChannelIdSelector(state));
@@ -16,9 +19,14 @@ const ChannelsContainer = () => {
     dispath(channelActions.switchChannel(id));
   };
 
+  const newChannelModalHandler = () => {
+    dispath(modalActions.showModal());
+    console.log(store.getState());
+  };
+
   return (
     <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
-      <ChannelsHeader />
+      <ChannelsHeader addChannelHandle={newChannelModalHandler} />
       <ChannelsList
         handleChoose={handleChoose}
         channels={channels}
