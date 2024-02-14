03fa86f3e984bf3358cbe3f7f6e2fe5311249257
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-14 20:30:30 +0300

    Add path for updateChanne

--- frontend/src/fetchApi/route.js
+++ frontend/src/fetchApi/route.js
@@ -4,5 +4,6 @@ export default {
   dataPath: () => [apiPath, 'data'].join('/'),
   loginPath: () => [apiPath, 'login'].join('/'),
   channelsPath: () => [apiPath, 'channels'].join('/'),
+  updateChannelPath: (id) => [apiPath, 'channels', id].join('/'),
   messagesPath: () => [apiPath, 'messages'].join('/'),
 };
