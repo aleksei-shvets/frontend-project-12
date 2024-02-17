f3d34cd4b7692eff13f1496a52972c6782d20db1
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-17 14:43:29 +0300

    Add locale and texts

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,34 @@
+export default {
+  translation: {
+    buttons: {
+      sendBtn: 'Отправить',
+      cancelBtn: 'Отменить',
+      loginBtn: 'Войти',
+      logoutBtn: 'Выйти',
+      registrationBtn: 'Зарегистрироваться',
+      deleteBtn: 'Удалить',
+      renameBtn: 'Переименовать',
+    },
+    placeholders: {
+      nickname: 'Ваш ник',
+      username: 'Имя пользователя',
+      password: 'Пароль',
+      confirmPassword: 'Подтвердите пароль',
+      message: 'Введите сообщение...',
+    },
+    formHeaders: {
+      registration: 'Регистрация',
+      login: 'Войти',
+    },
+    modalHeaders: {
+      renameModal: 'Переименовать канал',
+      removeModal: 'Удалить канал',
+      addModal: 'Добавить канал',
+    },
+    questions: {
+      registrationQuestion: 'Нет аккаунта?',
+      removeChannelQuestion: 'Уверены?',
+    },
+    channelsHeader: 'Каналы',
+  },
+};
