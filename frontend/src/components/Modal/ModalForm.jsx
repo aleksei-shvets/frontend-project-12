7fddce4508ebabdc64a073490f510e2b1e248cde
aleksei-shvetz <al.sv.dev@gmail.com>
2024-02-23 23:10:13 +0300

    Create ModalForm component

--- frontend/src/components/Modal/ModalForm.jsx
+++ frontend/src/components/Modal/ModalForm.jsx
@@ -13,16 +13,50 @@ import { modalActions, getUpdatedChannelId } from '../../store/slices/modalSlice
 import { channelActions, channelsSelector } from '../../store/slices/channelsSlice.js';
 import getShema from '../../validation/validation.js';
 
+const getDataFetch = (type) => {
+  const modalTypes = {
+    addingChannel: async ({ newChannel, token, dispatch }) => {
+      const response = await axios
+        .post(fetchRoutes.channelsPath(), newChannel, {
+          headers: {
+            Authorization: token.Authorization,
+          },
+        });
+      dispatch(channelActions.addChannel(response.data));
+      dispatch(channelActions.switchChannel(response.data.id));
+    },
+    renamingChannel: async ({ updatedChannelId, newChannel, token }) => {
+      await axios
+        .patch(fetchRoutes.updateChannelPath(updatedChannelId), newChannel, {
+          headers: {
+            Authorization: token.Authorization,
+          },
+        });
+    },
+  };
+  return modalTypes[type];
+};
+
 const ModalForm = ({ toastHandler, modalType }) => {
+  const token = getAuthHeader();
   const [connectionError, setConnectionError] = useState(null);
   const rollbar = useRollbar();
+  const dispatch = useDispatch();
+  const hideModal = () => dispatch(modalActions.hideModal());
   filterProfanity.loadDictionary('ru');
   const wordsFilter = (message) => filterProfanity.clean(message);
+  const inputEl = useRef(null);
+  useEffect(() => {
+    if (modalType !== 'removingChannel') {
+      inputEl.current.focus();
+      if (inputEl && inputEl.current.value !== '') {
+        inputEl.current.select();
+      }
+    }
+  }, []);
   const { t } = useTranslation();
-
   const channelNames = useSelector((state) => channelsSelector.selectAll(state))
     .map((channel) => channel.name);
-
   const { channelNameSchema } = getShema(t, channelNames);
 
   const updatedChannelId = useSelector(getUpdatedChannelId);
@@ -35,17 +69,6 @@ const ModalForm = ({ toastHandler, modalType }) => {
     return '';
   };
 
-  const inputEl = useRef(null);
-  useEffect(() => {
-    inputEl.current.focus();
-    if (inputEl.current.value !== '') {
-      inputEl.current.select();
-    }
-  }, []);
-
-  const dispatch = useDispatch();
-  const hideModal = () => dispatch(modalActions.hideModal());
-
   const connectionErrorEl = (isConnetionErr) => {
     if (isConnetionErr) {
       return (
@@ -57,29 +80,38 @@ const ModalForm = ({ toastHandler, modalType }) => {
     return null;
   };
 
+  const deleteChannelHandler = async (deletedChannelId, authToken, dispatcher) => {
+    await axios
+      .delete(fetchRoutes.updateChannelPath(deletedChannelId), {
+        headers: {
+          Authorization: authToken.Authorization,
+        },
+      });
+    dispatcher(channelActions.switchChannel('1'));
+    hideModal();
+    toastHandler(true);
+  };
+
   const formik = useFormik({
     initialValues: {
       nameInput: nameInitValue(),
     },
     validationSchema: channelNameSchema,
-    onSubmit: async (values) => {
-      const token = getAuthHeader();
+    onSubmit: (values) => {
       const channelName = wordsFilter(values.nameInput);
       formik.setSubmitting(true);
       try {
         const newChannel = {
           name: channelName,
         };
-        const response = await axios
-          .post(fetchRoutes.channelsPath(), newChannel, {
-            headers: {
-              Authorization: token.Authorization,
-            },
-          });
+        getDataFetch(modalType)({
+          newChannel,
+          token,
+          updatedChannelId,
+          dispatch,
+        });
         hideModal();
         toastHandler(true);
-        dispatch(channelActions.addChannel(response.data));
-        dispatch(channelActions.switchChannel(response.data.id));
         formik.resetForm();
       } catch (e) {
         if (e.isAxiosError) {
@@ -92,29 +124,50 @@ const ModalForm = ({ toastHandler, modalType }) => {
       }
     },
   });
-  console.log(formik.values.nameInput);
+
+  if (modalType !== 'removingChannel') {
+    return (
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Group className="mb-3" id="exampleForm.ControlInput1">
+          <Form.Label htmlFor="nameInput" className="visually-hidden" />
+          <InputGroup hasValidation>
+            <Form.Control
+              type="text"
+              name="nameInput"
+              id="nameInput"
+              onChange={formik.handleChange}
+              value={formik.values.nameInput}
+              isInvalid={!formik.isValid}
+              ref={inputEl}
+            />
+            <Form.Control.Feedback tooltip type="invalid">
+              {formik.errors.nameInput}
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+        <div className="d-flex justify-content-end">
+          <Button variant="outline-secondary" onClick={hideModal} className="me-2 ">{t('buttons.cancelBtn')}</Button>
+          <Button variant="secondary" type="submit">{t('buttons.sendBtn')}</Button>
+        </div>
+        {connectionErrorEl(connectionError)}
+      </Form>
+    );
+  }
   return (
     <Form onSubmit={formik.handleSubmit}>
-      <Form.Group className="mb-3" id="exampleForm.ControlInput1">
-        <Form.Label htmlFor="nameInput" className="visually-hidden" />
-        <InputGroup hasValidation>
-          <Form.Control
-            type="text"
-            name="nameInput"
-            id="nameInput"
-            onChange={formik.handleChange}
-            value={formik.values.nameInput}
-            isInvalid={!formik.isValid}
-            ref={inputEl}
-          />
-          <Form.Control.Feedback tooltip type="invalid">
-            {formik.errors.nameInput}
-          </Form.Control.Feedback>
-        </InputGroup>
-      </Form.Group>
+      {t('questions.removeChannelQuestion')}
       <div className="d-flex justify-content-end">
-        <Button variant="outline-secondary" onClick={hideModal} className="me-2 ">{t('buttons.cancelBtn')}</Button>
-        <Button variant="secondary" type="submit">{t('buttons.sendBtn')}</Button>
+        <Button variant="secondary" onClick={hideModal} className="me-2 ">{t('buttons.cancelBtn')}</Button>
+        <Button
+          variant="danger"
+          onClick={(e) => {
+            e.preventDefault();
+            deleteChannelHandler(updatedChannelId, token, dispatch);
+          }}
+          type="button"
+        >
+          {t('buttons.deleteBtn')}
+        </Button>
       </div>
       {connectionErrorEl(connectionError)}
     </Form>
--- frontend/src/components/Modal/RemoveChannelModal.jsx
+++ frontend/src/components/Modal/RemoveChannelModal.jsx
@@ -1,65 +1,16 @@
-import { Form } from 'react-bootstrap';
-import { useFormik } from 'formik';
-import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { useRollbar } from '@rollbar/react';
-import axios from 'axios';
-import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import fetchRoutes from '../../fetchApi/route.js';
-import getAuthHeader from '../../utils/getAuthHeader.js';
-import { isShownSelector, modalActions, getUpdatedChannelId } from '../../store/slices/modalSlice.js';
-import { channelActions } from '../../store/slices/channelsSlice.js';
+import { isShownSelector, modalActions } from '../../store/slices/modalSlice.js';
+import ModalForm from './ModalForm.jsx';
+
+const typePrope = 'removingChannel';
 
 const RemoveChannelModal = ({ toastHandler }) => {
-  const [connectionError, setConnectionError] = useState(null);
-  const rollbar = useRollbar();
   const { t } = useTranslation();
-  const updatedChannelId = useSelector(getUpdatedChannelId);
   const isShownModal = useSelector((state) => isShownSelector(state));
   const dispatch = useDispatch();
   const hideModal = () => dispatch(modalActions.hideModal());
-
-  const connectionErrorEl = (isConnetionErr) => {
-    if (isConnetionErr) {
-      return (
-        <div className="sm text-danger">
-          {t('errorMessages.incorrectSignup')}
-        </div>
-      );
-    }
-    return null;
-  };
-
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-    },
-    onSubmit: async () => {
-      const token = getAuthHeader();
-      formik.setSubmitting(true);
-      try {
-        await axios
-          .delete(fetchRoutes.updateChannelPath(updatedChannelId), {
-            headers: {
-              Authorization: token.Authorization,
-            },
-          });
-        hideModal();
-        dispatch(channelActions.switchChannel(1));
-        toastHandler(true);
-        formik.resetForm();
-      } catch (e) {
-        rollbar.error('Removing channel', e);
-        if (e.isAxiosError) {
-          setConnectionError(t('fetchErrors.connectionError'));
-        }
-      } finally {
-        formik.setSubmitting(false);
-      }
-    },
-  });
 
   return (
     <Modal
@@ -74,14 +25,7 @@ const RemoveChannelModal = ({ toastHandler }) => {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form onSubmit={formik.handleSubmit}>
-          {t('questions.removeChannelQuestion')}
-          <div className="d-flex justify-content-end">
-            <Button variant="secondary" onClick={hideModal} className="me-2 ">{t('buttons.cancelBtn')}</Button>
-            <Button variant="danger" type="submit">{t('buttons.deleteBtn')}</Button>
-          </div>
-          {connectionErrorEl(connectionError)}
-        </Form>
+        <ModalForm toastHandler={toastHandler} modalType={typePrope} />
       </Modal.Body>
     </Modal>
   );
