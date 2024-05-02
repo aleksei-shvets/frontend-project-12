9c04ab0ff29cbe2941e3b1f188a869fce7f2d662
aleksei-shvetz <al.sv.dev@gmail.com>
2024-05-02 21:35:43 +0300

    Delete commented code

--- frontend/src/store/slices/messagesSlice.js
+++ frontend/src/store/slices/messagesSlice.js
@@ -58,14 +58,6 @@ const messagesSlice = createSlice({
 export const { actions: messageActions } = messagesSlice;
 export const baseMessagesSelectors = messagesAdapter.getSelectors((state) => state.messages);
 
-// const selectAllMessages = (state) => baseMessagesSelectors.selectAll(state);
-
-/* export const selectMessagesByChannelId = createSelector(
-  [selectAllMessages, (_, currentChannelId) => currentChannelId],
-  (allMessages, currentChannelId) => allMessages
-    .filter((message) => Number(message.channelId) === Number(currentChannelId)),
-); */
-
 export const filtredMessages = createSelector(
   (state) => state,
   (state) => {
